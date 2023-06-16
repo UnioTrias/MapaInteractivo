967763f890d8f48aa38853853b3b0c1810a2cc42
Ter <114605209+UnioTrias@users.noreply.github.com>
2023-06-16 13:34:24 +0200

    inicio

---
+++ Script.js
@@ -0,0 +1,189 @@
+function out(){
+    document.getElementById('ciudad').src='img/ciudad950.png';
+}
+
+function donGato(){
+    document.getElementById('ciudad').src='img/Ciudad950_DonGato.png';
+}
+function donGato7(){
+    var texto='';
+    texto+='<p>Ese es Don Gato</p>';
+    texto+='<p>El casero del edificio<br> Estos son sus mandamientos:</p>';
+    texto+='<dl>';
+    texto+='<dt>Pescado</dt>';
+    texto+='<dd>El atún es su favorito pero le gustan todos</dd>';
+    texto+='<dt>Decir Gracias</dt>';
+    texto+='<dd>Don Gato siempre dice que la educación de una persona lo es todo</dd>';
+    texto+='<dt>Pagar el alquiler</dt>';
+    texto+='<dd>Puede que el más importante de todos.</dd>';
+    texto+='</dl>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto
+}
+
+function casaDonGato(){
+    document.getElementById('ciudad').src='img/Ciudad950_CasaDonGato.png';
+}
+function casaDonGato6(){
+    var texto='';
+    texto+='<p>¡Ahí vivo yo! <br><br> Mi cuarto está junto a la cocina y a veces voy a probar algo de lo que cocinan mis vecinos.</p>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function molino1(){
+    document.getElementById('ciudad').src='img/Ciudad950_molino1.png';
+}
+function molino11(){
+    var texto='';
+    texto+='<p>¡Mira, un molino de viento! <br><br> A ver cuantos más puedo encontrar.</p>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function molino2(){
+    document.getElementById('ciudad').src='img/Ciudad950_molino2.png';
+}
+function molino22(){
+    var texto='';
+    texto+='<p>¡Ajá! Otro más <br><br> ¿Cuántos molinos de viento llevo ya?.</p>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function molino3(){
+    document.getElementById('ciudad').src='img/Ciudad950_molino3.png';
+}
+function molino33(){
+    var texto='';
+    texto+='<p> Hay muchísimos molinos de viento <br><br> Quizá en el museo sepamos por qué hay tantos.</p>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function molinoAgua(){
+    document.getElementById('ciudad').src='img/Ciudad950_molinoAgua.png';
+}
+function molinoAgua4(){
+    var texto='';
+    texto+='<p>Cuenta la leyenda que esta ciudad antes no estaba en las alturas y por aquí pasaba un río. <br><br> Ahora se mueve cuando una nuve lo atraviesa bajo el puente.</p>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function steix(){
+    document.getElementById('ciudad').src='img/Ciudad950_steix.png';
+}
+function steix5(){
+    var texto='';
+    texto+='<p>Aquí está, el mejor restaurante de la ciudad, muchas horas en esa terraza.</p>';
+    texto+='<p>Si te fijas, desde casa se puede ver el menú del día.</p>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function ayunt(){
+    document.getElementById('ciudad').src='img/Ciudad950_ayunta.png';
+}
+function a8(){
+    var texto='';
+    texto+='<p>Ese es el ayuntamiento</p>';
+    texto+='<p>Parece que mañana hay reunión, veamos el orden del día.</p>';
+    texto+='<p>Orden del día. 2 de Oralune:</p>';
+    texto+='<ol>';
+    texto+='<li>Fiestas patronales</li>';
+    texto+='<li>Colocación de nuevo molino</li>';
+    texto+='<li>Obra de la calle Calla</li>';
+    texto+='<li>Ruegos y preguntas</li>';
+    texto+='</ol>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function iglesia(){
+    document.getElementById('ciudad').src='img/Ciudad950_iglesia.png';
+}
+function iglesia9(){
+    var texto='';
+    texto+='<p>Iglesia de Selune</p>';
+    texto+='<p>¿Has estado alguna vez dentro?, te lo recomiendo.</p>';
+    texto+='<img src="img/IglesiaP.jpg"></img>'
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function casaFlotante(){
+    document.getElementById('ciudad').src='img/Ciudad950_casaFlotante.png';
+}
+function casaFlotante10(){
+    var texto='';
+    texto+='<p>Eso es el museo</p>';
+    texto+='<p>Tiene toda la historia de esta ciudad.<br>Veamos que exposiciones hay ahora...</p>';
+    texto+='<table>'
+    texto+='<caption>Exposiciones</caption>'
+    texto+='<tr>'
+    texto+='<td bgcolor=#ffd8bf>Oralune</td>'
+    texto+='<td bgcolor=#ffd8bf>Therendor</td>'
+    texto+='<td bgcolor=#ffd8bf>Eyre</td>'
+    texto+='</tr>'
+    texto+='<tr>'
+    texto+='<td colspan="3" bgcolor=#c4fbf9>Historia de Ciudad Entropia</td>'
+    texto+=' </tr>'
+    texto+='<tr>'  
+    texto+='<td bgcolor=#fbc4fa>Vida en las alturas</td>'
+    texto+='<td bgcolor=#cdfbc4>El molino de viento</td>'
+    texto+='<td bgcolor="#fffdb3">¿Hay vida en tierra firme?</td>'
+    texto+='</tr>'
+    texto+='</table>'
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+function arbol(){
+    document.getElementById('ciudad').src='img/Ciudad950_arbol.png';
+}
+function arbol11(){
+    var texto='';
+    texto+='<p>El arbol Yggdrasil</p>';
+    texto+='<p>El más importante de Ciudad Entropia. Nos da el oxígeno necesario para vivir aquí arriba.<br>Cada año se hace el festival bajo sus ramas.</p>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+} 
+
+function menu(){
+    document.getElementById('ciudad').src='img/Ciudad950_menuSteix.png';
+}
+function menu12(){
+    var texto='';
+    texto+='<ul>';
+    texto+='Primeros:';
+    texto+='<li>Ensalada Steix</li>';
+    texto+='<li>Sopa de fideos</li>';
+    texto+='<br>';
+    texto+=' Segundos:';
+    texto+='<li>Pollo en salsa</li>';
+    texto+='<li>Atún con tomatico</li>';
+    texto+='</ul>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+} 
+
+function persona(){
+    document.getElementById('ciudad').src='img/Ciudad950_persona.png';
+}
+function persona13(){
+    var texto='';
+    texto+='<p>Ella es mi vecina</p>';
+    texto+='<p>Siempre está ahí sentada, no se para qué paga la habitación de Don Gato.<br> Prepara galletas cada mañana. mmm... <br> ¿Cuántos años tendrá?</p>';
+    texto+='<p align="right">&dArr;</p>';
+    document.getElementById('contenedor').innerHTML=texto;
+}
+
+
+function salir(){
+    document.getElementById('contenedor').innerHTML='';
+}
+
+ 
+
+
